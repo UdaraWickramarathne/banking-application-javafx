024265f02831dfa08ddd4e72231f65d9e23ee4c2
UdaraWickramarathne <bimsaraudara25@gmail.com>
2024-07-17 20:16:00 +0530

    Login form creation done

--- src/main/java/com/paylink/paylink/controllers/LoginController.java
+++ src/main/java/com/paylink/paylink/controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.paylink.paylink.controllers;
 
+import javafx.scene.control.*;
+
 public class LoginController {
+    public TextField username_txt;
+    public PasswordField password_txt;
+    public CheckBox password_chk;
+    public Button login_btn;
+    public Label forget_lbl;
+    public Label error_lbl;
 }
