86ddadd8feae5ce1f3c0ec6d9f3ca00b2877b282
UdaraWickramarathne <bimsaraudara25@gmail.com>
2024-07-19 13:40:53 +0530

    Client dashbord ui finished

--- src/main/java/com/paylink/paylink/Main.java
+++ src/main/java/com/paylink/paylink/Main.java
@@ -3,7 +3,10 @@ package com.paylink.paylink;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application{
 
@@ -13,5 +16,9 @@ public class Main extends Application{
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
